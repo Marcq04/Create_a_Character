5ea597619369f3fa5af35d134c739a4b62832f18
Marcq04 <156446124+Marcq04@users.noreply.github.com>
2025-05-26 20:17:51 -0400

    removed params for user queries

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -90,11 +90,11 @@ const typeDefs = gql`
         getAcceptedSubmissionsByUser(userId: ID!): [Submission]
         getLikes(submissionId: ID!): [Like]
         getComments(submissionId: ID!): [Comment]
-        getUserCharacters(userId: ID!): [Character]
-        getUserBounties(userId: ID!): [Bounty]
-        getUserSubmissions(userId: ID!): [Submission]
-        getUserLikes(userId: ID!): [Like]
-        getUserComments(userId: ID!): [Comment]
+        getUserCharacters: [Character]
+        getUserBounties: [Bounty]
+        getUserSubmissions: [Submission]
+        getUserLikes: [Like]
+        getUserComments: [Comment]
     }
 
     type Mutation {
